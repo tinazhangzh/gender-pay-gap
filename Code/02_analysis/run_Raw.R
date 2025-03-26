625d5fe8937909d80a482270a64d29893d655025
Zihan Zhang <148819581+tinazhangzh@users.noreply.github.com>
2025-03-26 14:11:40 -0400

    Create run_Raw.R

---
+++ Code/02_analysis/run_Raw.R
@@ -0,0 +1,8 @@
+# 1. Raw Model
+# Wage Difference in Percetage 
+average_salary_by_gender <- data %>%
+  group_by(Gender) %>%
+  summarise(mean_salary = mean(Salary, na.rm = TRUE))
+male_salary <- average_salary_by_gender$mean_salary[average_salary_by_gender$Gender == 0]
+female_salary <- average_salary_by_gender$mean_salary[average_salary_by_gender$Gender == 1]
+gender_pay_gap <- (male_salary - female_salary) / male_salary * 100
